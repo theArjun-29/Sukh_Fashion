47ede6d76f780666eccb923b6cca4ffebd37ce22
Arjun Singh <165902340+theArjun-29@users.noreply.github.com>
2024-04-04 19:59:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// const bar = document.getElementById('bar')
+// const nav = document.getElementById('navbar')
+
+// if (bar) {
+//     bar.addEventListener('click', () => {
+//         nav.classList.add('active');
+//     })
+// }
+
+
+
+// let navbar = document.querySelector("#navbar");
+
+// navbar.addEventListener("click" , () => {
+//     document.querySelector("navbar").classList.add("active")
+//     console.log("active");
+// })
+
+// let navLinks = document.getElementById("navLinks");
+
+// function showMenu(){
+//     navLinks.style.right = "0";
+// }
